622066f774ffe626e81dd0a457554c7c10502fec
supertinypeaunt <1176032017@qq.com>
2019-12-03 00:42:54 +0800

    字符串转驼峰

--- source/_posts/test.js
+++ source/_posts/test.js
@@ -20,11 +20,12 @@ function ribArr(arr) {
   return arr.sort((a, b) => b - a)
 }
 
+console.log(ribArr([12, 2, 2, 234, 4, 6, 6, 7, 6])) //[ 234, 12, 7, 6, 4, 2 ]
+
+
+
 /**
  * 用 js 实现随机选取 10–100 之间的 10 个数字，存入一个数组，并排序。
- * @param { number } num 
- * @param { number } max 
- * @param { number } min 
  */
 function getRandomArr(num, max, min) {
   const arr = [];
@@ -33,3 +34,24 @@ function getRandomArr(num, max, min) {
   }
   return arr.sort((a, b) => a - b)
 }
+
+console.log(getRandomArr(10, 100, 10)) //[ 33, 45, 48, 65, 68, 77, 80, 90, 91, 95 ]
+
+
+/**
+ * 已知有字符串 foo=”get-element-by-id”,写一个 function 将其转化成 驼峰表示法”getElementById”。
+ */
+
+function transformHump(params) {
+  //   切割字符串
+  const paramsArr = params.split('-')
+    //   存储首个子串
+  params = paramsArr[0]
+    //   将非子串首字母转化为大写
+  for (let index = 1; index < paramsArr.length; index++) {
+    const item = paramsArr[index]
+    params += item.charAt(0).toUpperCase() + item.slice(1)
+  }
+  return params
+}
+console.log(transformHump('get-element-by-id')) //getElementById
